9658a54df801dd53a19fed094fe2fd9ab33d1a71
Alex <neglux.agency@gmail.com>
2026-01-22 17:19:32 +0100

    fail111

--- app/actions/manage-subscription.ts
+++ app/actions/manage-subscription.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe'
 import { headers } from 'next/headers'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2024-12-18.acacia',
+    apiVersion: '2025-12-15.clover',
 })
 
 export async function manageSubscription(email: string) {
