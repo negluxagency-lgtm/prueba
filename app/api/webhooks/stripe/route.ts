80a32c6d2a4c7fb846ec175263d468bd2454ee1e
Alex <neglux.agency@gmail.com>
2026-01-24 06:03:12 +0100

    hhg

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -7,7 +7,7 @@ import { createClient } from '@supabase/supabase-js';
 // ------------------------------------------------------
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2023-10-16',
+    apiVersion: '2025-12-15.clover',
     typescript: true,
 });
 
